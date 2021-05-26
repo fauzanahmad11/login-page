a8fd65e2ba153e7fcbeeb860845614bc93a410ca
Fauzan Ahmad <fauzanahmadgojeck@gmail.com>
2021-05-26 20:59:45 +0700

    initial commit

---
+++ layout.js
@@ -0,0 +1,5 @@
+// ====================Pre loader====================
+const getLoader = document.querySelector('section.preloader');
+window.onload = function () {
+    getLoader.style.display = "none";
+}
